0230733379cc74a429934b8e24e8d9b488336fe3
mantanquzzmo <quzzmo@hotmail.com>
2020-05-11 16:46:04 -0400

    finishes up setup

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const driver = require('dotenv').config()
+require("dotenv").config();
 
 const app = express();
-key = process.env.SERVER_KEY
-console.log(process.env.SERVER_KEY)
+key = process.env.SERVER_KEY;
 
-mongoose.set('useUnifiedTopology', true);
-mongoose.connect(`mongodb+srv://mantanquzzmo:${key}@cluster0-eh0if.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true });
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect(
+  `mongodb+srv://mantanquzzmo:${key}@cluster0-eh0if.mongodb.net/test?retryWrites=true&w=majority`,
+  { useNewUrlParser: true }
+);
 const db = mongoose.connection;
 
-db.once('open', () => {
-  console.log('Mongoose default connection is open');
+db.on("error", (error) => console.error(error));
+db.once("open", () => {
+  console.log("Mongoose default connection is open");
 });
 
-app.listen(3001, () => console.log("Football server is on."));
+app.use(express.json())
+
+app.listen(3001, () => console.log("Football server is running."));
